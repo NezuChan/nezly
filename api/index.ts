69226a89f67389df35b8afea15c6d0dee903abd5
KagChi <59391215+KagChi@users.noreply.github.com>
2022-07-27 20:51:34 +0700

    fix: correctly src folder path

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "src/app.module";
+import { AppModule } from "../src/app.module";
 
 const NezlyApp = (async () => {
     const app = await NestFactory.create(AppModule);
