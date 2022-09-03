7084d23094f7ac54c27553666d1979925cc3cb14
KagChi <59391215+KagChi@users.noreply.github.com>
2022-09-03 11:57:54 +0700

    fix: cannot lookup when nodeName not provided

--- src/app.node.service.ts
+++ src/app.node.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable max-len */
 import { Injectable } from "@nestjs/common";
 import { cast } from "@sapphire/utilities";
@@ -6,7 +7,7 @@ import { cast } from "@sapphire/utilities";
 export class AppNodeService {
     public getLavalinkNode(nodeName?: string, excludeNode?: string): NodeOptions {
         const nodes: NodeOptions[] = JSON.parse(cast(process.env.LAVALINKS ?? []));
-        return nodes.filter(node => (excludeNode ? node.name !== excludeNode : node.name === nodeName))[nodeName ? 0 : Math.floor(Math.random() * (excludeNode ? nodes.length - 1 : nodes.length))];
+        return nodes.filter(node => (excludeNode ? node.name !== excludeNode : nodeName ? node.name === nodeName : true))[nodeName ? 0 : Math.floor(Math.random() * (excludeNode ? nodes.length - 1 : nodes.length))];
     }
 }
 
