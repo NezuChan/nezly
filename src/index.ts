2b50e840cc54f368923765ee783965d9ccb1e281
KagChi <yonuxbot@gmail.com>
2022-12-22 14:35:18 +0700

    feat: initial v2 rewrite

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+    logger: {
+        name: "nezu",
+        timestamp: true,
+        level: process.env.NODE_ENV === "production" ? "info" : "trace",
+        formatters: {
+            bindings: () => ({
+                pid: "Nezly"
+            })
+        },
+        transport: {
+            targets: [
+                { target: "pino-pretty", level: process.env.NODE_ENV === "production" ? "info" : "trace", options: { translateTime: "SYS:yyyy-mm-dd HH:MM:ss.l o" } }
+            ]
+        }
+    }
+});
+
+fastify.get("/", () => ({ message: "LavaLink REST Proxy API" }));
+
+await fastify.listen({ host: "0.0.0.0", port: Number(process.env.PORT ?? 3000) });
