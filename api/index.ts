b538bc5e8d487d710095e480afd187afdb83a59a
KagChi <yonuxbot@gmail.com>
2022-12-22 15:23:13 +0700

    feat: implement all basic routes

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,13 @@
+/* eslint-disable no-nested-ternary */
+import "reflect-metadata";
+import "dotenv/config";
+
 import Fastify from "fastify";
 import PinoPretty from "pino-pretty";
 import Pino from "pino";
+import { NodeOptions } from "./types";
+import { cast } from "@sapphire/utilities";
+import { REST } from "@kirishima/rest";
 
 const fastify = Fastify({
     logger: Pino({
@@ -15,6 +22,57 @@ const fastify = Fastify({
     }, PinoPretty())
 });
 
+function getLavalinkNode(nodeName?: string, excludeNode?: string): REST {
+    const nodes: NodeOptions[] = JSON.parse(cast(process.env.LAVALINKS ?? []));
+    const firstNode = nodes
+        .filter(node => (excludeNode ? node.name !== excludeNode : nodeName ? node.name === nodeName : true))[
+            nodeName ? 0 : Math.floor(Math.random() * (excludeNode ? nodes.length - 1 : nodes.length))
+        ];
+    return new REST(`${firstNode.secure ? "https" : "http"}://${firstNode.host}`)
+        .setAuthorization(firstNode.auth);
+}
+
 fastify.get("/", () => ({ message: "LavaLink REST Proxy API" }));
+
+fastify.get("/loadtracks", {
+    schema: {
+        querystring: {
+            identifier: { type: "string" }
+        }
+    }
+}, async (request, reply) => {
+    const node = getLavalinkNode();
+    const { identifier } = request.query as { identifier: string };
+
+    const source = identifier.split(":")[0];
+    const query = identifier.split(":")[1];
+
+    const result = await node.loadTracks(source ? { source, query } : identifier);
+    return reply.send(result);
+});
+
+fastify.get("/decodetrack", {
+    schema: {
+        querystring: {
+            track: { type: "string" }
+        }
+    }
+}, async (request, reply) => {
+    const node = getLavalinkNode();
+    const { track } = request.query as { track: string };
+
+    const result = await node.decodeTracks(track);
+    return reply.send(result);
+});
+
+fastify.post("/decodetracks", {
+
+}, async (request, reply) => {
+    const node = getLavalinkNode();
+    const { tracks } = request.body as { tracks: string };
+
+    const result = await node.decodeTracks(tracks);
+    return reply.send(result);
+});
 
 void fastify.listen({ host: "0.0.0.0", port: Number(process.env.PORT ?? 3000) });
---
+++ api/types.ts
@@ -0,0 +1,6 @@
+export interface NodeOptions {
+    name: string;
+    host: string;
+    auth: string;
+    secure?: boolean;
+}
